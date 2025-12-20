263b057d97bdfb3b58298d6be469843fa50349e0
matsumo0922 <caios.system@gmail.com>
2025-12-21 01:44:32 +0900

    remove unused Compose dependencies and update Kotlin & ksp versions

--- build-logic/src/main/kotlin/primitive/KmpComposePlugin.kt
+++ build-logic/src/main/kotlin/primitive/KmpComposePlugin.kt
@@ -1,14 +1,8 @@
 package primitive
 
 import me.matsumo.travelog.android
-import me.matsumo.travelog.androidTestImplementation
-import me.matsumo.travelog.debugImplementation
-import me.matsumo.travelog.implementation
-import me.matsumo.travelog.library
-import me.matsumo.travelog.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.dependencies
 
 class KmpComposePlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -20,15 +14,6 @@ class KmpComposePlugin : Plugin<Project> {
 
             android {
                 buildFeatures.compose = true
-            }
-
-            dependencies {
-                val bom = libs.library("compose-bom")
-
-                implementation(project.dependencies.platform(bom))
-                implementation(libs.library("compose-ui-tooling-preview"))
-                debugImplementation(libs.library("compose-ui-tooling"))
-                androidTestImplementation(project.dependencies.platform(bom))
             }
         }
     }
