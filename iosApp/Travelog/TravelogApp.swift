8e2ac10d19a7b54a7deff59241e91551f5e82b57
matsumo0922 <caios.system@gmail.com>
2025-12-22 00:52:13 +0900

    feat: initialize Travelog iOS app project
    - Add Xcode project configuration and workspace setup for Travelog.
    - Include app icon and accent color assets.
    - Implement `TravelogApp` and `ContentView` with SwiftUI.

---
+++ iosApp/Travelog/TravelogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelogApp.swift
+//  Travelog
+//
+//  Created by 松本 大智 on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
